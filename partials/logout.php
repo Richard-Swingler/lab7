65ffe5f9d10505a4c58e1a0cb129b0ad5e92ce5e
Richard-Swingler <rs14g12@soton.ac.uk>
2013-11-21 15:28:03 +0000

    Added welcome message and logout link

--- partials/logout.php
+++ partials/logout.php
@@ -1,1 +1,5 @@
-<em>partials/logout.php: This is a placeholder for the logout form.</em>
+<?php 	require_once('includes/predispatch.php');
+
+	echo "<p>Welcome ".$_SESSION['username'].", if this is not you please logout</p>";
+	echo "<p><a href='../lab7/process-logout.php'>Logout</a></p>";
+	
